51e9644946ebf62014cd824e36423fb0cb3edf2c
Leray <seb.leray@outlook.fr>
2020-02-05 10:03:16 +0100

    add phone number

--- src/main/java/istic/m2cyber/vet/security_api/controllers/AuthenticationController.java
+++ src/main/java/istic/m2cyber/vet/security_api/controllers/AuthenticationController.java
@@ -1,16 +1,21 @@
 package istic.m2cyber.vet.security_api.controllers;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -18,6 +23,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.people.v1.PeopleService;
+import com.google.api.services.people.v1.model.Person;
+import com.google.api.services.people.v1.model.PhoneNumber;
 
 import istic.m2cyber.vet.security_api.models.Log;
 import istic.m2cyber.vet.security_api.models.User;
@@ -32,6 +44,18 @@ public class AuthenticationController {
 	
 	@Autowired
 	private LogService logservice;
+	
+	@Value("${spring.security.oauth2.client.registration.google.client-id}")
+	private String client_id;
+	
+	@Value("${spring.security.oauth2.client.registration.google.client-secret}")
+	private String client_secret;
+	
+	private final OAuth2AuthorizedClientService authorizedClientService;
+
+	public AuthenticationController(OAuth2AuthorizedClientService authorizedClientService) {
+		this.authorizedClientService = authorizedClientService;
+	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Authentication authentication, Model model) {
@@ -108,6 +132,25 @@ public class AuthenticationController {
 		String url = "http://127.0.0.1:8080";
 		RedirectView view = new RedirectView(url);
 		return view;
+	}
+	
+	private List<PhoneNumber> getPhoneNumbers(Authentication authentication) throws IOException {
+		OAuth2AuthenticationToken oauth2AuthToken = ((OAuth2AuthenticationToken) authentication);
+
+		OAuth2AccessToken access_token = this.authorizedClientService
+				.loadAuthorizedClient(oauth2AuthToken.getAuthorizedClientRegistrationId(), oauth2AuthToken.getName())
+				.getAccessToken();
+
+		GoogleCredential credential = new GoogleCredential.Builder().setTransport(new NetHttpTransport())
+				.setJsonFactory(new JacksonFactory()).setClientSecrets(client_id, client_secret).build()
+				.setAccessToken(access_token.getTokenValue());
+
+		PeopleService peopleService = new PeopleService.Builder(new NetHttpTransport(), new JacksonFactory(), credential).build();
+
+		Person profile = peopleService.people().get("people/me").setRequestMaskIncludeField("person.phone_numbers").execute();
+
+		List<PhoneNumber> phoneList = profile.getPhoneNumbers();
+		return phoneList;
 	}
 
 }
